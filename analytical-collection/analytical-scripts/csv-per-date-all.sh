17d258cefdb485932664645ecb16673ddab66797
Avocado generator <Avocado@example.email>
2023-10-16 18:19:36 +0000

    Fix shell scripts python

--- analytical-collection/analytical-scripts/csv-per-date-all.sh
+++ analytical-collection/analytical-scripts/csv-per-date-all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/lenta.csv"   --path "../analytical-data/csv/per-days/lenta" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/regnum.csv"  --path "../analytical-data/csv/per-days/regnum" --datetime-format "%a, %d %b %Y %H:%M:%S %Z" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/ren.csv"     --path "../analytical-data/csv/per-days/ren" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/rg.csv"      --path "../analytical-data/csv/per-days/rg" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/ria.csv"     --path "../analytical-data/csv/per-days/ria" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/rt.csv"      --path "../analytical-data/csv/per-days/rt" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/tass.csv"    --path "../analytical-data/csv/per-days/tass" "$@"
-/Users/dandelion/.pyenv/shims/python3 csv-per-date.py --csv "../analytical-data/csv/vseruss.csv" --path "../analytical-data/csv/per-days/vseruss" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/lenta.csv"   --path "../analytical-data/csv/per-days/lenta" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/regnum.csv"  --path "../analytical-data/csv/per-days/regnum" --datetime-format "%a, %d %b %Y %H:%M:%S %Z" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/ren.csv"     --path "../analytical-data/csv/per-days/ren" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/rg.csv"      --path "../analytical-data/csv/per-days/rg" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/ria.csv"     --path "../analytical-data/csv/per-days/ria" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/rt.csv"      --path "../analytical-data/csv/per-days/rt" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/tass.csv"    --path "../analytical-data/csv/per-days/tass" "$@"
+python3 csv-per-date.py --csv "../analytical-data/csv/vseruss.csv" --path "../analytical-data/csv/per-days/vseruss" "$@"
