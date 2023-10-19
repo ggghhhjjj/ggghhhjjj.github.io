d39514b066a7f00eb5f8ade896d10a777dc1df08
pocahontas <10704633+ggghhhjjj@users.noreply.github.com>
2023-10-19 22:57:01 +0300

    Add day statistics

--- analytical-collection/analytical-scripts/csv-rows-count-all.sh
+++ analytical-collection/analytical-scripts/csv-rows-count-all.sh
@@ -2,66 +2,82 @@
 
 current_date=$(date +'%Y-%m-%d')
 
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/lenta/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/lenta/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/lenta/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/lenta/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/lenta/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/lenta/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/lenta/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/lenta/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/regnum/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/regnum/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/regnum/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/regnum/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/regnum/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/regnum/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/regnum/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/regnum/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/ren/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/ren/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/ren/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/ren/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/ren/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/ren/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/ren/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/ren/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/rg/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/rg/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/rg/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/rg/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/rg/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/rg/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/rg/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/rg/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/ria/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/ria/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/ria/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/ria/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/ria/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/ria/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/ria/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/ria/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/rt/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/rt/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/rt/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/rt/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/rt/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/rt/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/rt/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/rt/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/tass/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/tass/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/tass/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/tass/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/tass/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/tass/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/tass/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/tass/$current_date/tag_statistics.csv" "$@"
 #
-python3 csv-rows-count.py --sources "../analytical-data/csv/per-days/vseruss/$current_date/tag_експерт.csv" \
+python3 csv-rows-count.py --sources \
+"../analytical-data/csv/per-days/vseruss/$current_date/publications.csv" \
+"../analytical-data/csv/per-days/vseruss/$current_date/tag_експерт.csv" \
 "../analytical-data/csv/per-days/vseruss/$current_date/tag_захарова.csv" \
 "../analytical-data/csv/per-days/vseruss/$current_date/tag_зеленски.csv" \
 "../analytical-data/csv/per-days/vseruss/$current_date/tag_макгрегор.csv" \
 "../analytical-data/csv/per-days/vseruss/$current_date/tag_путин.csv" \
---labels "експерт" "захарова" "зеленски" "макгрегор" "путин" \
+--labels "общо за деня" "експерт" "захарова" "зеленски" "макгрегор" "путин" \
 --target "../analytical-data/csv/per-days/vseruss/$current_date/tag_statistics.csv" "$@"
