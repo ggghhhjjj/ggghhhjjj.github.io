3958c5fbc138e2bc545262e69e2efba4bc695cdd
pocahontas <10704633+ggghhhjjj@users.noreply.github.com>
2023-10-24 00:21:46 +0300

    Fix CSV with only header

--- analytical-collection/analytical-scripts/rss-curl-to-csv.py
+++ analytical-collection/analytical-scripts/rss-curl-to-csv.py
@@ -6,7 +6,8 @@ Args:
     --url: RSS url
     --csv: CSV file path name. Script will not append *.csv extension
     --headers: Optional headers separated by spaces. Ex., "accept-language: en-GB,en-US;q=0.9,en;q=0.8"
-    --datetime-format: Optional datetime format string 
+    --datetime-format: Optional datetime format string
+    --debug: Optional print trace massages
     """
 
 import argparse
@@ -20,6 +21,9 @@ from io import StringIO
 from typing import List
 
 import shared
+
+
+HAS_DEBUG = False
 
 
 """ Define the format of the datetime string
@@ -49,7 +53,7 @@ def parse_arguments():
         default=[
             "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "accept-language: en-GB,en-US;q=0.9,en;q=0.8",
-            "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+            "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
         ],
         help="Optional headers separated by spaces. Ex., 'accept-language: en-GB,en-US;q=0.9,en;q=0.8'.",
     )
@@ -59,6 +63,12 @@ def parse_arguments():
         required=False,
         default="%a, %d %b %Y %H:%M:%S %z",
         help="Optional datetime format.",
+    )
+
+    parser.add_argument(
+        "--debug",
+        default="False",
+        help="Optional print trace massages",
     )
 
     return parser.parse_args()
@@ -95,18 +105,24 @@ def read_last_record(filename: str) -> List[str]:
 
 
 def only_new_items(sorted_items, pub_date_str: str, datetime_format: str):
-    from_date = datetime.strptime(pub_date_str, datetime_format)
-
-    for index, item1 in enumerate(sorted_items):
-        item_date = datetime.strptime(item1["pubDateStr"], datetime_format)
-        if item_date > from_date:
-            return sorted_items[index:]
-    return None
+    try:
+        from_date = datetime.strptime(pub_date_str, datetime_format)
+    except ValueError as e:
+        return None
+    else:
+        for index, item1 in enumerate(sorted_items):
+            item_date = datetime.strptime(item1["pubDateStr"], datetime_format)
+            if item_date > from_date:
+                return sorted_items[index:]
+        return None
 
 
 def make_curl_request(curl_command):
     try:
-        # Execute the 'curl' command and capture the output
+        global HAS_DEBUG
+        if HAS_DEBUG:
+            print (curl_command)
+
         result = subprocess.run(
             curl_command,
             shell=True,
@@ -115,10 +131,12 @@ def make_curl_request(curl_command):
             stderr=subprocess.PIPE,
         )
         if result.returncode == 0:
+            if HAS_DEBUG:
+                print (f"result.returncode={result.returncode}")
+
             return result.stdout
         else:
-            print("Curl command failed with an error:")
-            print(result.stderr)
+            print(f"Curl command failed with return code {result.returncode} and error: {result.stderr}")
             return None
     except Exception as e:
         print("Error executing the 'curl' command:", str(e))
@@ -126,12 +144,20 @@ def make_curl_request(curl_command):
 
 
 def parse_rss(response_text):
+    global HAS_DEBUG
     rss_items = []
 
     if response_text:
         rss_feed = feedparser.parse(response_text)
 
+        if HAS_DEBUG:
+            print(f"rss_feed.entries size is {len(rss_feed.entries)}")
+
         for entry in rss_feed.entries:
+
+            if HAS_DEBUG:
+                print(f"Prcoessing {entry}")
+
             description = shared.utils.escape_nl_tab(entry.get('description')) if entry.get('description') is not None else ''
 
             item_dict = {
@@ -142,6 +168,9 @@ def parse_rss(response_text):
                 "Description": description,
             }
 
+            if HAS_DEBUG:
+                print(f"Parsed values {item_dict}")
+
             rss_items.append(item_dict)
 
     if len(rss_items) == 0:
@@ -150,13 +179,30 @@ def parse_rss(response_text):
     return rss_items
 
 
+MAGIC_CSV_HEADER_STR="ubDateStr,Title,Link,Description"
+
+def is_row_csv_header(last_csv_line_array):
+    raw_string = ','.join(last_csv_line_array)
+    return MAGIC_CSV_HEADER_STR == raw_string
+
 def save_to_csv(rss_items, csv_filename, datetime_format):
-    # Sort the rss_items list by pubDate in ascending order
+    global HAS_DEBUG
     sorted_items = sorted(rss_items, key=lambda x: x["pubDate"])
+
+    if HAS_DEBUG:
+        print(f"\nsorted_items: {sorted_items}")
 
     last_csv_line_array = read_last_record(csv_filename)
 
-    if last_csv_line_array is None:
+    if HAS_DEBUG:
+        print(f"\nlast_csv_line_array: {last_csv_line_array}")
+
+    is_csv_header = is_row_csv_header(last_csv_line_array)
+
+    if HAS_DEBUG:
+        print(f"\nlast_csv_line_array is csv header: {is_csv_header}")
+
+    if last_csv_line_array is None or is_csv_header:
         file_mode = "w"
         hasHeader = True
     else:
@@ -167,6 +213,8 @@ def save_to_csv(rss_items, csv_filename, datetime_format):
         )
 
     if sorted_items is None:
+        if HAS_DEBUG:
+            print("WARNING: Because sorted_items is none script finished execution. CSV file has not changed.")
         sys.exit()
 
     os.makedirs(os.path.dirname(csv_filename), exist_ok=True)
@@ -188,8 +236,13 @@ def save_to_csv(rss_items, csv_filename, datetime_format):
 
 
 def main(args):
+    global HAS_DEBUG
+    HAS_DEBUG = bool(args.debug) if args.debug is not None else False
+
     rss_url = args.url
+
     print (f"Processing {rss_url} ...")
+
     csv_filename = args.csv
 
     formatted_elements = ['-H "{}"'.format(element) for element in args.headers]
@@ -198,6 +251,9 @@ def main(args):
     curl_command = f'curl --compressed -sk "{rss_url}" {headers_str}'
 
     response_text = make_curl_request(curl_command)
+
+    if HAS_DEBUG:
+        print(f"response_text={response_text}")
 
     rss_items = parse_rss(response_text)
 
